8fbfa330091e467611effe0720fa3991e9205dd3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-11 16:21:02 +0800

    prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+}
