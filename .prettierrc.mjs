2fa623c35c4e99402b928201f3e4e97bb5cd2241
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-11 16:29:23 +0800

    install tailwind prettier sort plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,7 +3,11 @@
  * @type {import("prettier").Config}
  */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: [
+    'prettier-plugin-astro',
+    // must come last https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+    'prettier-plugin-tailwindcss',
+  ],
   overrides: [
     {
       files: '*.astro',
