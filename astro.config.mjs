1f046896e56272715c231b06a59237797af01785
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-11 16:55:09 +0800

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 import netlify from '@astrojs/netlify'
 
