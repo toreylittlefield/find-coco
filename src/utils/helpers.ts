2eb3f742a054e1fe880cc6ed782ff29feb4f5920
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-13 16:53:52 +0800

    shadcn merge util

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,12 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export const typeSafeObjectEntries = <T extends Record<string, unknown>>(
   obj: T,
 ) => {
   return Object.entries(obj) as Array<[keyof T, T[keyof T]]>
+}
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs))
 }
