611e10dd1e1287fb6f1186448889599884633d28
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-15 18:56:23 +0800

    messenger link

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,12 +1,14 @@
 import AstroIcon from '@/assets/icons/astro.svg?raw'
 import EnglishIcon from '@/assets/icons/english.svg?raw'
 import IndonesiaFlagIcon from '@/assets/icons/indonesia-flag.svg?raw'
+import MessengerIcon from '@/assets/icons/messenger.svg?raw'
 import WhatsAppIcon from '@/assets/icons/whatsapp.svg?raw'
 
 export const ICONS_MAP = {
   astro: AstroIcon,
   english: EnglishIcon,
   indonesia: IndonesiaFlagIcon,
+  messenger: MessengerIcon,
   whatsapp: WhatsAppIcon,
 } as const
 
