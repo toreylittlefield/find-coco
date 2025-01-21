9582e2179489e565a4987890b48120f308f654ab
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-21 17:08:24 +0800

    add meta pixel

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,7 +1,8 @@
 interface ImportMetaEnv {
-  readonly PUBLIC_WHATSAPP_PHONE_NUMBER: string
   readonly PUBLIC_FB_MESSENGER_URL: string
   readonly PUBLIC_GTAG: string
+  readonly PUBLIC_META_PIXEL: string
+  readonly PUBLIC_WHATSAPP_PHONE_NUMBER: string
 }
 
 interface ImportMeta {
