f767c0edb3b0914bc6b88f64985ed6d0165cca18
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-23 15:30:29 +0800

    update astro config with reverse proxy for pixel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,24 @@ import { defineConfig } from 'astro/config'
 
 import netlify from '@astrojs/netlify'
 
+import { loadEnv } from 'vite'
+
+const NODE_ENV = process.env.NODE_ENV
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.',
+  )
+}
+
+const env = loadEnv(NODE_ENV, process.cwd(), '')
+const PUBLIC_SITE_DOMAIN = env.PUBLIC_SITE_DOMAIN
+
+if (!PUBLIC_SITE_DOMAIN) {
+  throw new Error(
+    'The PUBLIC_SITE_DOMAIN environment variable is required but was not specified.',
+  )
+}
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -16,11 +34,24 @@ export default defineConfig({
       redirectToDefaultLocale: true,
     },
   },
-
+  site: PUBLIC_SITE_DOMAIN,
   integrations: [
     tailwind({ applyBaseStyles: false }),
     partytown({
-      config: { forward: ['dataLayer.push'] },
+      config: {
+        forward: ['dataLayer.push', 'fbq'],
+        resolveUrl: function (url) {
+          if (url.hostname === 'connect.facebook.net') {
+            const baseURL = new URL(PUBLIC_SITE_DOMAIN)
+            const netlifyFunctionPath = '/.netlify/functions/partytown-proxy'
+            const proxyUrl = new URL(netlifyFunctionPath, baseURL)
+
+            proxyUrl.searchParams.append('url', url.href)
+            return proxyUrl
+          }
+          return url
+        },
+      },
     }),
   ],
   adapter: netlify(),
