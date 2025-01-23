507c750414b8cb9fc3ae4cb904d00de2633f6b3e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2025-01-23 18:26:49 +0800

    hardcode the pixel value for now

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,7 +1,6 @@
 interface ImportMetaEnv {
   readonly PUBLIC_FB_MESSENGER_URL: string
   readonly PUBLIC_GTAG: string
-  readonly PUBLIC_META_PIXEL: string
   readonly PUBLIC_WHATSAPP_PHONE_NUMBER: string
 }
 
